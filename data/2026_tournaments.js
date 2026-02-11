5275b67d76ec9da618e69747c6290859ca99af5a
withm2k <withm2k@gmail.com>
2026-02-11 06:33:18 +0000

    feat: Add current work

---
+++ data/2026_tournaments.js
@@ -0,0 +1,306 @@
+// 2026 BWF Tournament Data - Real Data from TournamentSoftware
+// Last Updated: February 11, 2026
+// Week 7 - 2026 Season
+
+const tournaments2026 = [
+    {
+        name: "PETRONAS Malaysia Open 2026",
+        startDate: "2026-01-06",
+        endDate: "2026-01-11",
+        city: "Kuala Lumpur",
+        country: "Malaysia",
+        region: "Asia",
+        grade: "Super 1000",
+        prizePool: "$1,300,000"
+    },
+    {
+        name: "YONEX-SUNRISE India Open 2026",
+        startDate: "2026-01-13",
+        endDate: "2026-01-18",
+        city: "New Delhi",
+        country: "India",
+        region: "Asia",
+        grade: "Super 750",
+        prizePool: "$850,000"
+    },
+    {
+        name: "DAIHATSU Indonesia Masters 2026",
+        startDate: "2026-01-20",
+        endDate: "2026-01-25",
+        city: "Jakarta",
+        country: "Indonesia",
+        region: "Asia",
+        grade: "Super 500",
+        prizePool: "$420,000"
+    },
+    {
+        name: "PRINCESS SIRIVANNAVARI Thailand Masters 2026",
+        startDate: "2026-01-27",
+        endDate: "2026-02-01",
+        city: "Bangkok",
+        country: "Thailand",
+        region: "Asia",
+        grade: "Super 300",
+        prizePool: "$210,000"
+    },
+    {
+        name: "YONEX German Open 2026",
+        startDate: "2026-02-24",
+        endDate: "2026-03-01",
+        city: "Mülheim",
+        country: "Germany",
+        region: "Europe",
+        grade: "Super 300",
+        prizePool: "$210,000"
+    },
+    {
+        name: "YONEX All England Open Badminton Championships 2026",
+        startDate: "2026-03-10",
+        endDate: "2026-03-15",
+        city: "Birmingham",
+        country: "England",
+        region: "Europe",
+        grade: "Super 1000",
+        prizePool: "$1,300,000"
+    },
+    {
+        name: "Azerbaijan International 2026",
+        startDate: "2026-02-03",
+        endDate: "2026-02-08",
+        city: "Baku",
+        country: "Azerbaijan",
+        region: "Europe",
+        grade: "International Challenge"
+    },
+    {
+        name: "Uganda International Challenge 2026",
+        startDate: "2026-02-18",
+        endDate: "2026-02-22",
+        city: "Kampala",
+        country: "Uganda",
+        region: "Africa",
+        grade: "International Challenge"
+    },
+    {
+        name: "SBH Singapore International Challenge 2026",
+        startDate: "2026-02-24",
+        endDate: "2026-03-01",
+        city: "Singapore",
+        country: "Singapore",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "Brazil International Challenge 2026",
+        startDate: "2026-03-24",
+        endDate: "2026-03-29",
+        city: "São Paulo",
+        country: "Brazil",
+        region: "Pan Am",
+        grade: "International Challenge"
+    },
+    {
+        name: "CIPUTRA HANOI - YONEX SUNRISE Vietnam International Challenge 2026",
+        startDate: "2026-03-24",
+        endDate: "2026-03-29",
+        city: "Hanoi",
+        country: "Vietnam",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "YONEX Estonian International 2026",
+        startDate: "2026-01-08",
+        endDate: "2026-01-11",
+        city: "Tallinn",
+        country: "Estonia",
+        region: "Europe",
+        grade: "International Series"
+    },
+    {
+        name: "VICTOR Swedish Open 2026",
+        startDate: "2026-01-15",
+        endDate: "2026-01-18",
+        city: "Uppsala",
+        country: "Sweden",
+        region: "Europe",
+        grade: "International Series"
+    },
+    {
+        name: "Sri Lanka International Series 2026",
+        startDate: "2026-03-09",
+        endDate: "2026-03-15",
+        city: "Colombo",
+        country: "Sri Lanka",
+        region: "Asia",
+        grade: "International Series"
+    },
+    {
+        name: "TOYOTA Phuket International Challenge 2026",
+        startDate: "2026-06-09",
+        endDate: "2026-06-14",
+        city: "Phuket",
+        country: "Thailand",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "SMART Philippines International Challenge 2026",
+        startDate: "2026-07-28",
+        endDate: "2026-08-02",
+        city: "Manila",
+        country: "Philippines",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "Japan Open 2026",
+        startDate: "2026-07-21",
+        endDate: "2026-07-26",
+        city: "Tokyo",
+        country: "Japan",
+        region: "Asia",
+        grade: "Super 750",
+        prizePool: "$850,000"
+    },
+    {
+        name: "PETRONAS Malaysia International Challenge 2026",
+        startDate: "2026-08-11",
+        endDate: "2026-08-16",
+        city: "Kuala Lumpur",
+        country: "Malaysia",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "BWF World Championships 2026",
+        startDate: "2026-08-17",
+        endDate: "2026-08-23",
+        city: "New Delhi",
+        country: "India",
+        region: "Asia",
+        grade: "World Championships"
+    },
+    {
+        name: "Korea Open 2026",
+        startDate: "2026-08-25",
+        endDate: "2026-08-30",
+        city: "Seoul",
+        country: "South Korea",
+        region: "Asia",
+        grade: "Super 500",
+        prizePool: "$420,000"
+    },
+    {
+        name: "Indonesia Super 100 I 2026",
+        startDate: "2026-09-01",
+        endDate: "2026-09-06",
+        city: "TBC",
+        country: "Indonesia",
+        region: "Asia",
+        grade: "Super 100"
+    },
+    {
+        name: "China Open 2026",
+        startDate: "2026-09-15",
+        endDate: "2026-09-20",
+        city: "Changzhou",
+        country: "China",
+        region: "Asia",
+        grade: "Super 1000",
+        prizePool: "$1,300,000"
+    },
+    {
+        name: "Asian Games 2026 Nagoya",
+        startDate: "2026-09-19",
+        endDate: "2026-10-04",
+        city: "Nagoya",
+        country: "Japan",
+        region: "Asia",
+        grade: "Major Event (Grade 1)"
+    },
+    {
+        name: "Guatemala International Challenge 2026",
+        startDate: "2026-09-29",
+        endDate: "2026-10-04",
+        city: "Guatemala City",
+        country: "Guatemala",
+        region: "Pan Am",
+        grade: "International Challenge"
+    },
+    {
+        name: "Denmark Open 2026",
+        startDate: "2026-10-13",
+        endDate: "2026-10-18",
+        city: "Odense",
+        country: "Denmark",
+        region: "Europe",
+        grade: "Super 750",
+        prizePool: "$850,000"
+    },
+    {
+        name: "Malaysia Super 100 2026",
+        startDate: "2026-10-13",
+        endDate: "2026-10-18",
+        city: "Kuala Lumpur",
+        country: "Malaysia",
+        region: "Asia",
+        grade: "Super 100"
+    },
+    {
+        name: "French Open 2026",
+        startDate: "2026-10-20",
+        endDate: "2026-10-25",
+        city: "Paris",
+        country: "France",
+        region: "Europe",
+        grade: "Super 750",
+        prizePool: "$850,000"
+    },
+    {
+        name: "Peru International Challenge 2026",
+        startDate: "2026-10-21",
+        endDate: "2026-10-25",
+        city: "Lima",
+        country: "Peru",
+        region: "Pan Am",
+        grade: "International Challenge"
+    },
+    {
+        name: "INFOSYS FOUNDATION India International Challenge 2026",
+        startDate: "2026-11-03",
+        endDate: "2026-11-08",
+        city: "Bengaluru",
+        country: "India",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "Indonesia International Challenge II 2026",
+        startDate: "2026-11-17",
+        endDate: "2026-11-22",
+        city: "TBC",
+        country: "Indonesia",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "TOYOTA Chiang Mai International Challenge 2026",
+        startDate: "2026-11-24",
+        endDate: "2026-11-29",
+        city: "Chiang Mai",
+        country: "Thailand",
+        region: "Asia",
+        grade: "International Challenge"
+    },
+    {
+        name: "BWF World Tour Finals 2026",
+        startDate: "2026-12-09",
+        endDate: "2026-12-13",
+        city: "Hangzhou",
+        country: "China",
+        region: "Asia",
+        grade: "BWF Finals",
+        prizePool: "$2,500,000"
+    }
+];
